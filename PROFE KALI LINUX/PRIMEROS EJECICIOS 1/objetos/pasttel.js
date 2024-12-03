454e9e709939f955e2ca7e661f02d0fd8de58b31
administrador <nachacotp1972@gmail.com>
2024-12-03 14:43:39 +0100

    .

---
+++ PROFE KALI LINUX/PRIMEROS EJECICIOS 1/objetos/pasttel.js
@@ -0,0 +1,20 @@
+
+let pastel = {
+    nombre: 'caña',
+    calorias: 1000,
+    peso: 250,
+    ingrediente1: ['harina',
+                    'levadura',
+                    'azucar',
+                    'huevo',
+                    'chocolate'],
+    precio: 2.5,
+    cortar: function(numTrozos) {
+        console.log(`Cortando la caña en ${numTrozos} trozos`);
+    },
+    comer: {persona, pagaDinero) => {
+        console.log('${persona} paga ${pagaDinero} por pastel ')
+    }
+
+
+}
