c5eff9a685fee7253a77aaacdd09955e67222d48
administrador <nachacotp1972@gmail.com>
2025-10-08 14:41:21 +0200

    .

---
+++ DAM2/ProyectoFinal/src/main/java/org/example/juegos/buscaminas/Calculos/CalculosGeneral.java
@@ -0,0 +1,15 @@
+package org.example.juegos.buscaminas.Calculos;
+
+public class CalculosGeneral {
+
+    public static int hola;
+    /*
+    hacer un for para cuando coloque las minas en lugares random por ejemplo quiero 20 minas en posiciones
+    random con la altura y la anchura del tablero por ejemplo 10 x 10
+    - peticion: 20 minas en posicion , anchura y altura 10 x 10
+    - salida: un arraylist con arraylist de 2 posicones dentro ( Arraylist<Arraylist<Integer>> respuesta = [[1,8][5,6]...20 veces]
+
+    lo aria con un for que se repita asta que se haga las posciones random del numero de minas y para que no se haga repetido
+    si encuentro esas 2 mismas posiciones de le suma al bucle otra vuelta para que haga otra posicon no repetida 
+     */
+}
