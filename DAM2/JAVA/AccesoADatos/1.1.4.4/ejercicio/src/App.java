cfd0d9e2ba239714fdf80b05393d2e6acd9b911c
sajitario2004 <nachacotp1972@gmail.com>
2025-10-14 16:25:18 +0200

    .

---
+++ DAM2/JAVA/AccesoADatos/1.1.4.4/ejercicio/src/App.java
@@ -0,0 +1,12 @@
+
+
+import com.google.gson.Gson;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Gson hola = new Gson();
+
+        System.out.println(hola);
+    }
+}
