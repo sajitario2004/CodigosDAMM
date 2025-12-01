cfd080c51f240df28f39d7b1b16a5351c8ae53bc
administrador <nachacotp1972@gmail.com>
2025-12-01 09:41:56 +0100

    .

--- DAM2/PROGRAMCIO/PYTHON/HLC/Mod4 - Sec 4-1/ejer1.py
+++ DAM2/PROGRAMCIO/PYTHON/HLC/Mod4 - Sec 4-1/ejer1.py
@@ -2,6 +2,6 @@ def make_shirt(size, message):
     print(f"Shirt withc siza: {size}")
     print(f"Message to print: {message}")
     
-def __main__():
-    make_shirt(size="XL", message="No se traducir")
+
+make_shirt(size="XL", message="No se traducir")
     
